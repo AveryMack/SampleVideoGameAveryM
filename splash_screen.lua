fe95854789415f2ae71d9f25e81d5542ba391c9c
Avery Mack <avery.mack@ocsbstudent.ca>
2018-04-10 19:57:30 -0400

    changes

--- splash_screen.lua
+++ splash_screen.lua
@@ -1,8 +1,8 @@
 -----------------------------------------------------------------------------------------
 --
 -- splash_screen.lua
--- Created by: Your Name
--- Date: Month Day, Year
+-- Created by: Avery Mack
+-- Date: April 10, 2018
 -- Description: This is the splash screen of the game. It displays the 
 -- company logo that...
 -----------------------------------------------------------------------------------------
